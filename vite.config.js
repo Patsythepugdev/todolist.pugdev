56728f6b065352eeb954024dec82c5184820198b
Patsy The Pug_dev <mettabhavanatrance@gmail.com>
2025-07-28 16:08:54 -0600

    Agrega workflow para despliegue automático

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist/', // 👈 importante: nombre EXACTO de tu repositorio
+  base: '/todolist/', // 👈 nombre exacto de tu repositorio en GitHub
 })
