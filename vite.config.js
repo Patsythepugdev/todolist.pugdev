bb61fc3721d7dfe51e6c31159f9ce166296e9b82
Patsy The Pug_dev <mettabhavanatrance@gmail.com>
2025-07-28 16:48:21 -0600

    Corrige importación de defineConfig para Vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist/', // 👈 nombre exacto de tu repositorio en GitHub
-})
+  base: '/todolist/', // cambia esto al nombre del repo si es distinto
+});
