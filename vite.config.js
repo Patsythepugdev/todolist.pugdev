f147a1385314430c6a4d7338ab06897fc1ab4778
Patsy The Pug_dev <mettabhavanatrance@gmail.com>
2025-07-29 08:23:53 +0000

    Cambios locales antes de rebase

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist-patsy/', //
+  base: '/todolist-patsy/', // 👈 ESTE debe coincidir exactamente con el nombre del repo
 })
