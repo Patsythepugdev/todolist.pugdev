bfb7f510e1c7940cb26e7152346fc0b5afae7760
Patsy The Pug_dev <mettabhavanatrance@gmail.com>
2025-07-29 07:49:45 +0000

    Fix: use correct GitHub Pages deploy workflow for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 👇 Este nombre exacto debe coincidir con el repositorio de GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist-patsy/', 
+  base: '/todolist-patsy/', //
 })
