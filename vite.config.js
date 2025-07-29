cc0aae12cdc69c4286b6d8d157abe9eaeac7c8cd
Patsy The Pug_dev <mettabhavanatrance@gmail.com>
2025-07-29 06:29:38 +0000

    Corrige base de Vite para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 👇 Este nombre exacto debe coincidir con el repositorio de GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/todolist/', //https://github.com/alejandrabarcena/todolist
+  base: '/todolist-patsy/', 
 })
